d67fa07b94822ddc1d29a9f3475610029b2f4447
schweinwerfer <YnbVUEvmyEsY9uAHOvzk>
2018-10-15 16:28:51 +0200

    Setup

--- src/main/java/de/ora/gaston/core/Main.java
+++ src/main/java/de/ora/gaston/core/Main.java
@@ -1,16 +1,17 @@
 package de.ora.gaston.core;
 
-import net.dv8tion.jda.client.events.group.GroupJoinEvent;
-import net.dv8tion.jda.client.events.group.GroupUserJoinEvent;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.DisconnectEvent;
 import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
@@ -41,22 +42,43 @@ public class Main extends ListenerAdapter {
     }
 
     @Override
-    public void onGroupJoin(GroupJoinEvent event) {
-        super.onGroupJoin(event);
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        final User author = event.getUser();
+        if (author.isBot()) {
+            return;
+        }
+
+        String joinMessage = "Hallo %user%! Willkommen auf %guild%!";
+        if (joinMessage.equals("0")) {
+            return;
+        } else {
+            final Guild guild = event.getGuild();
+            TextChannel messageChannel = guild.getDefaultChannel();
+            if (messageChannel == null)
+                return;
+            final Member member = event.getMember();
+            joinMessage = joinMessage.replace("%user%", member.getAsMention());
+            joinMessage = joinMessage.replace("%guild%", guild.getName());
+            messageChannel.sendMessage(joinMessage).queue();
+        }
     }
 
-    @Override
-    public void onGroupUserJoin(GroupUserJoinEvent event) {
-        super.onGroupUserJoin(event);
-    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot()) {
+        final User author = event.getAuthor();
+        if (author.isBot()) {
             return;
         }
         if (event.getMessage().getContentRaw().equalsIgnoreCase("!metta")) {
-            event.getChannel().sendMessage("Hallo!").queue();
+            final MessageChannel channel = event.getChannel();
+            String message = "Hallo %user%! Willkommen auf %guild%. Stell dich doch kurz auf %introchannel% vor";
+            message = message.replace("%user%", author.getAsMention());
+            message = message.replace("%guild%", event.getGuild().getName());
+            final List<TextChannel> channels = event.getGuild().getTextChannelsByName("vorstellung", true);
+            message = message.replace("%introchannel%", channels.get(0).getAsMention());
+            //channel.sendMessage("Hallo " + author.getAsMention() + "! Ich bin ein Bot.").queue();
+            channel.sendMessage(message).queue();
         }
     }
 }
